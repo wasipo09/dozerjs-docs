664270fc2816b415b0ec2d009ec3f7fdf2105d88
fluidbyte <kent@fluidbyte.net>
2014-02-16 21:43:40 +0000

    Added model template

---
+++ models/template.js
@@ -0,0 +1,17 @@
+// Model Object
+// Representation of data structure and any validation on properties.
+module.exports = {
+
+  // Example Properties:
+
+  // name: 'string',
+  // email: 'email',
+  // password: 'string',
+  // tags: 'array',
+  // handle: 'alphanum'
+
+  // The above would utilize the internal validation. The validation
+  // schemas such as 'string' and 'array' use native JS detection, while
+  // more custom validations can be set via the regEx object in /config.js
+
+};
